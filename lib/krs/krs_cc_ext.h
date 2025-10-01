41c5aa1b3bbca60fcdf0ca0e56592b8179ca77dd
Kris Scott <kscott91@gmail.com>
2025-09-30 20:13:45 -0700

    Add nodiscard compiler detection

--- lib/krs/krs_cc_ext.h
+++ lib/krs/krs_cc_ext.h
@@ -1,8 +1,10 @@
 #ifndef KRS_CC_EXT_H_
 #define KRS_CC_EXT_H_
 
-// TODO: Detect if extension is available and no-op if it isn't
-
+#if defined(__GNUC__) || defined(__clang__)
 #define nodiscard __attribute__((warn_unused_result))
+#else
+#define nodiscard
+#endif
 
 #endif
